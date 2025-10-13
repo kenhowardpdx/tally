c5e151e17d59e8756648074b1ba53f69b29b25a0
Ken Howard <ken@kenhoward.dev>
2025-10-13 09:47:12 -0700

    fix: remove duplicate output definitions from bastion module

--- infra/modules/bastion/main.tf
+++ infra/modules/bastion/main.tf
@@ -12,9 +12,3 @@ resource "aws_instance" "bastion" {
   # Stop instance when not needed to save costs
 }
 
-output "bastion_public_ip" {
-  value = aws_instance.bastion.public_ip
-}
-output "bastion_id" {
-  value = aws_instance.bastion.id
-}
